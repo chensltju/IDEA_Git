f8866b2b4ec0ca359c7f4c50813cc501f69d9bb6
tom_glb <goodMoring_glb@atguigu.com>
2021-03-04 10:51:21 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author csl
+ * @date 2021/3/4-10:41
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
